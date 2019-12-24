e82429d7d7fcecb2d469727f31de8fcb9cf1f469
Bahadir Hocamoglu <bahadirhocamoglu@gmail.com>
2019-12-24 05:27:46 +0300

    add etag to list-annotations-by-article endpoint

--- app/annotation-server/controllers/annotations/retrieveAnnotationsByArticle.js
+++ app/annotation-server/controllers/annotations/retrieveAnnotationsByArticle.js
@@ -1,8 +1,12 @@
 const { Annotation, ETag } = require("../../models");
 
 module.exports = async (req, res, next) => {
-  const instances = await Annotation.find({
+  let instances = await Annotation.find({
     articleId: req.params.articleId
   }).lean();
+  instances = await Promise.all(instances.map(async anno => {
+    const { etag } = await ETag.findOne({ annotationId: anno._id }).lean();
+    return { ...anno, ETag: etag };
+  }));
   res.json(instances);
 };
